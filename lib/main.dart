1f6c0169508d941b32a81c20a208942e0f24ae54
ishh91 <ishantsharma9105@gmail.com>
2022-04-21 18:41:50 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/notes_list.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+void main() async {
+  await GetStorage.init();
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'TODO APP',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: NoteList(),
+    );
+  }
+}
